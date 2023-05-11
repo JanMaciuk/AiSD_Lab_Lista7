7ee15868f97be21cb04a89cb127367525cfcb6b8
JanMaciuk <38810823+JanMaciuk@users.noreply.github.com>
2023-05-11 10:09:02 +0200

    Add comments for simplest algorithms

--- src/SimpleSorting.java
+++ src/SimpleSorting.java
@@ -2,6 +2,8 @@ public class SimpleSorting {
 
 
     //Sortowania proste:
+
+    //Dla każdego elementu po kolei szukam odpowiedniego miejsca w posortowanym zakresie żeby go wstawić.
     public static long InsertSort(int [] lista){
         System.out.print("InsertSort: ");
         long startTime = System.nanoTime();
@@ -17,28 +19,29 @@ public class SimpleSorting {
         return System.nanoTime() - startTime;
     }
 
+    //Szukam najmniejszego nieposortowanego elementu i zamieniam go z kolejnym nieposortowanym.
     public static long SelectSort(int[] lista) {
         System.out.print("SelectSort: ");
         long startTime = System.nanoTime();
         int length = lista.length;
 
         for (int sortedIndex = 0; sortedIndex < length; ++sortedIndex)  {
-            int maxIndex = sortedIndex;
+            int minIndex = sortedIndex;
 
             for (int sprawdzanyIndex = sortedIndex+1; sprawdzanyIndex < length; sprawdzanyIndex++) {
-                if (lista[sprawdzanyIndex] < lista[maxIndex]) { // Szukam najmniejszego elementu
-                    maxIndex = sprawdzanyIndex;
+                if (lista[sprawdzanyIndex] < lista[minIndex]) { // Szukam najmniejszego elementu
+                    minIndex = sprawdzanyIndex;
                 }
             }
 
-            int replacedElement = lista[maxIndex]; // Zamieniam najmniejszy element z następnym nieposortowanym.
-            lista[maxIndex] = lista[sortedIndex];
+            int replacedElement = lista[minIndex]; // Zamieniam najmniejszy element z następnym nieposortowanym.
+            lista[minIndex] = lista[sortedIndex];
             lista[sortedIndex] = replacedElement;
         }
         return System.nanoTime() - startTime;
     }
 
-
+    //Po kolei dla każdego elementu porównuje czy jest mniejszy od poprzedniego, jeżeli tak to zamieniam je miejscami.
     static long bubbleSort(int[] lista) {
         System.out.print("BubbleSort: ");
         long startTime = System.nanoTime();
@@ -46,7 +49,7 @@ public class SimpleSorting {
 
         for (int i = 0; i < size; i++) {
 
-            for (int j = size; j > i; j--) { // > i bo porównujemy do elementów już posortowanych
+            for (int j = size; j > i; j--) { // > i bo nie porównujemy do elementów już posortowanych
 
                 if (lista[j] < lista[j - 1]) {
 
